b4da05d520bffce610a7ea962f62360df16234b4
Nguyễn Thành Tâm <tamfly_production@yahoo.com>
2017-11-25 23:12:25 +0700

    MyDeal: TamNT initial commit

--- Web/config/prod.env.js
+++ Web/config/prod.env.js
@@ -1,4 +1,9 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+	API_KEY: '"AIzaSyAnd1FqXoXbmyukq-X9vZI4eVDleN3KuBY"',
+	AUTH_DOMAIN: '"my-deal-a129f.firebaseapp.com"',
+	DATABASE_URL: '"https://my-deal-a129f.firebaseio.com"',
+	PROJECT_ID: '"my-deal-a129f"',
+	STORAGE_BUCKET: '"my-deal-a129f.appspot.com"',
+	MESSAGING_SENDER_ID: '"694349101108"'
 }
--- Web/src/main.js
+++ Web/src/main.js
@@ -1,15 +1,27 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import * as firebase from "firebase";
 
+var config = {
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	databaseURL: process.env.DATABASE_URL,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
+
+Vue.prototype.$firebase = firebase;
+Vue.prototype.$auth = firebase.auth();
+Vue.prototype.$db = firebase.database();
+Vue.prototype.$storage = firebase.storage();
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  render: h => h(App)
 })
